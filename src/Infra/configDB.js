3810136d3bf9336daae73add2ec1333452d95e28
AnaVitoriaFreitasUSP <anavitoria_gouvea@usp.br>
2021-05-22 14:11:08 -0300

    adicionado env variables

--- src/Infra/configDB.js
+++ src/Infra/configDB.js
@@ -1,5 +1,7 @@
+require('dotenv/config');
+
 const proConfig = {
-    connectionString: "postgres://uhcfpauwitnqmb:d4c0bc4a8018b97db48978e543ae84f9d9f03547724f47f3613abeb24280c1ba@ec2-54-158-232-223.compute-1.amazonaws.com:5432/d95qsia49lljk2",
+    connectionString: process.env.CONNECTION_URI,
     ssl: {
       rejectUnauthorized: false
     }
