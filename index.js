8264aadd69dc6a8c6f6705acb5ff895f641aa0f5
AnaVitoriaFreitasUSP <anavitoria_gouvea@usp.br>
2021-05-22 08:40:33 -0300

    testando nova rota

--- index.js
+++ index.js
@@ -25,6 +25,24 @@ app.get('/', async(req, res) => {
   }
 })
 
+app.get('/ana', (req, res) =>{
+    try{
+        const Pool = require("pg").Pool;
+        const proConfig = {
+          connectionString: "postgres://uhcfpauwitnqmb:d4c0bc4a8018b97db48978e543ae84f9d9f03547724f47f3613abeb24280c1ba@ec2-54-158-232-223.compute-1.amazonaws.com:5432/d95qsia49lljk2",
+          ssl: {
+            rejectUnauthorized: false
+          }
+        }
+
+        const pool = new Pool(proConfig);
+
+        res.send("Rota ana configurada!");
+    }catch(Exception){
+        res.send(Exception);
+    }
+})
+
 app.listen(process.env.PORT || 3000, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
