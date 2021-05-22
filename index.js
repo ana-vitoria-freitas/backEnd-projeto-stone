3613ebb1d9d5a32c75578aace298262a3bfd3adf
AnaVitoriaFreitasUSP <anavitoria_gouvea@usp.br>
2021-05-22 08:48:33 -0300

    colocando as configs do banco na pasta Infra

--- index.js
+++ index.js
@@ -3,15 +3,12 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const proConfig = require('./src/Infra/config');
+
 app.get('/', async(req, res) => {
   try {
     const Pool = require("pg").Pool;
-    const proConfig = {
-      connectionString: "postgres://uhcfpauwitnqmb:d4c0bc4a8018b97db48978e543ae84f9d9f03547724f47f3613abeb24280c1ba@ec2-54-158-232-223.compute-1.amazonaws.com:5432/d95qsia49lljk2",
-      ssl: {
-        rejectUnauthorized: false
-      }
-    }
+
 
     const pool = new Pool(proConfig);
 
@@ -27,13 +24,6 @@ app.get('/', async(req, res) => {
 
 app.get('/ana', (req, res) =>{
     try{
-        const Pool = require("pg").Pool;
-        const proConfig = {
-          connectionString: "postgres://uhcfpauwitnqmb:d4c0bc4a8018b97db48978e543ae84f9d9f03547724f47f3613abeb24280c1ba@ec2-54-158-232-223.compute-1.amazonaws.com:5432/d95qsia49lljk2",
-          ssl: {
-            rejectUnauthorized: false
-          }
-        }
 
         const pool = new Pool(proConfig);
 
---
+++ src/Infra/config.js
@@ -0,0 +1,8 @@
+const proConfig = {
+  connectionString: "postgres://uhcfpauwitnqmb:d4c0bc4a8018b97db48978e543ae84f9d9f03547724f47f3613abeb24280c1ba@ec2-54-158-232-223.compute-1.amazonaws.com:5432/d95qsia49lljk2",
+  ssl: {
+    rejectUnauthorized: false
+  }
+}
+
+module.exports = proConfig;
